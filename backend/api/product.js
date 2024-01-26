aa27495b5e385aa68850566ea41aa57571859d1a
RamonBarros <89661742+RamonBarros@users.noreply.github.com>
2024-01-26 14:26:07 -0300

    ajuste na api de produtos para salvar/modificar informações de um produto, incluindo a(s) URL(s) das imagens do produto.

--- backend/api/product.js
+++ backend/api/product.js
@@ -1,45 +1,73 @@
 module.exports = app => {
-    const {existsOrError,notExistsOrError} = app.api.validation
+    const { existsOrError, notExistsOrError } = app.api.validation
 
-    const save = (req,res) => {
-        const product = {...req.body}
-        if(req.params.id){
-            product.id = req.params.id
+    const save = async (req, res) => {
+        const info = { ...req.body }
+        if (req.params.id) {
+            info.product.id = req.params.id
         }
+        console.log(info)
 
-        try{
-            existsOrError(product.name, 'Nome Não Informado')
-            existsOrError(product.short_description, 'Descrição Curta Não Informada')
-            existsOrError(product.long_description, 'Descrição Longa Não Informada')
-            existsOrError(product.shortVideoUrl, 'Url do video curto Não Informada')
-            existsOrError(product.longVideoUrl, 'Url do video longo Não Informada')
-            existsOrError(product.categoryId, 'Categoria Não Informada')
-            existsOrError(product.price, 'Preço Não Informado')
-        }catch(msg){
+        try {
+            existsOrError(info.product.name, 'Nome Não Informado')
+            existsOrError(info.product.short_description, 'Descrição Curta Não Informada')
+            existsOrError(info.product.long_description, 'Descrição Longa Não Informada')
+            existsOrError(info.product.shortVideoUrl, 'Url do video curto Não Informada')
+            existsOrError(info.product.longVideoUrl, 'Url do video longo Não Informada')
+            existsOrError(info.product.categoryId, 'Categoria Não Informada')
+            existsOrError(info.product.stock, 'Quantidade em estoque Não Informado')
+            existsOrError(info.product.product_height, 'Altura do Produto Não informada')
+            existsOrError(info.product.product_width, 'Largura do Produto Não Informada')
+            existsOrError(info.product.product_length, 'Comprimento do Produto Não Informado')
+            existsOrError(info.product.product_weight, 'Peso Liquido do Produto Não Informado')
+            existsOrError(info.product.package_height, 'Altura da Embalagem do Produto Não Informada')
+            existsOrError(info.product.package_weight, 'Peso Bruto do Produto Não Informado')
+            existsOrError(info.product.package_length, 'Comprimento da Embalagem do Produto Não Informado')
+            existsOrError(info.product.package_width, 'Largura da Embalagem do Produto Não Informado')
+
+            if (info.product.id) {
+                console.log("entrou")
+
+                app.db('products')
+                    .update(info.product)
+                    .where({ id: info.product.id })
+            } else {
+                const [productId] = await app.db('products')
+                    .insert(info.product).returning("id")
+                info.product.id = productId.id
+            }
+            if (info.images.length > 0 && info.images != null) {
+                const imageData = info.images.map(url => ({
+                    product_id: info.product.id,
+                    url: url
+                }));
+                console.log(imageData)
+                
+                await app.db('product_images').where({product_id: info.product.id }).del();
+                
+                await app.db('product_images').insert(imageData)
+            }
+
+            res.status(200).send();
+
+        } catch (msg) {
             res.status(400).send(msg)
         }
-        if(product.id){
-            app.db('products')
-                .update(product)
-                .where({id: product.id})
-                .then(_=> res.status(200).send())
-                .catch(err=> res.status(500).send(err))
-        } else{
-            app.db('products')
-                .insert(product)
-                .then(_=> res.status(200).send())
-                .catch(err=> res.status(500).send(err))
-        }
+
+
+
+
+
     }
 
     const remove = async (req, res) => {
-        try{
+        try {
             const rowsDeleted = await app.db('products')
-                .where({id: req.params.id}).del()
-            existsOrError(rowsDeleted,'Produto Não Foi Encontrado.')
+                .where({ id: req.params.id }).del()
+            existsOrError(rowsDeleted, 'Produto Não Foi Encontrado.')
 
             res.status(200).send()
-        }catch(msg){
+        } catch (msg) {
             res.status(500).send(msg)
         }
     }
@@ -52,29 +80,29 @@ module.exports = app => {
         const count = parseInt(result.count)
 
         app.db('products')
-            .select('id','name', 'imageUrl','stock')
+            .select('id', 'name', 'imageUrl', 'stock')
             .then(products => res.json(products))
-            .catch(err =>res.status(500).send(err))
+            .catch(err => res.status(500).send(err))
     }
 
-    const getById = async (req,res) => {
+    const getById = async (req, res) => {
         app.db('products')
-            .where({id: req.params.id})
+            .where({ id: req.params.id })
             .then(product => product = res.json(product))
             .catch(err => res.status(500).send(err))
     }
 
-    const getByCategoryCart = async (req,res) => {
+    const getByCategoryCart = async (req, res) => {
         const categoryId = req.params.id
-        
+
         app.db('products')
-            .select('name','imageUrl','price','id')
-            .where({categoryId:categoryId})
+            .select('name', 'imageUrl', 'price', 'id')
+            .where({ categoryId: categoryId })
             .then(products => res.json(products))
-            .catch(err => res.status(500).send(err)) 
+            .catch(err => res.status(500).send(err))
 
-    } 
+    }
 
-   
-    return {save, remove, get, getById, getByCategoryCart}
+
+    return { save, remove, get, getById, getByCategoryCart }
 }
