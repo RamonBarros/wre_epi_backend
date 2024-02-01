d1489d6e47c17d2f3f6e001e9d7a373e7b30d81f
RamonBarros <89661742+RamonBarros@users.noreply.github.com>
2024-02-01 17:24:59 -0300

    ajustes na tela de adicionar,remover e alterar produtos.

--- backend/api/product.js
+++ backend/api/product.js
@@ -3,87 +3,163 @@ module.exports = app => {
 
     const save = async (req, res) => {
         const info = { ...req.body }
-        if (req.params.id) {
-            info.product.id = req.params.id
-        }
-        console.log(info)
+
+        const keys = Object.keys(info);
+        const valuesExceptLast = keys.slice(0, -1).map(key => info[key]);
+        const lastKey = keys.slice(-1)[0];
+        const lastValue = info[lastKey];
+
+        const product = Object.fromEntries(keys.slice(0, -1).map(key => [key, info[key]]));
+        const images = { [lastKey]: lastValue };
+
+        // console.log(product);
+        console.log(images.images);
 
         try {
-            existsOrError(info.product.name, 'Nome Não Informado')
-            existsOrError(info.product.short_description, 'Descrição Curta Não Informada')
-            existsOrError(info.product.long_description, 'Descrição Longa Não Informada')
-            existsOrError(info.product.shortVideoUrl, 'Url do video curto Não Informada')
-            existsOrError(info.product.longVideoUrl, 'Url do video longo Não Informada')
-            existsOrError(info.product.categoryId, 'Categoria Não Informada')
-            existsOrError(info.product.stock, 'Quantidade em estoque Não Informado')
-            existsOrError(info.product.product_height, 'Altura do Produto Não informada')
-            existsOrError(info.product.product_width, 'Largura do Produto Não Informada')
-            existsOrError(info.product.product_length, 'Comprimento do Produto Não Informado')
-            existsOrError(info.product.product_weight, 'Peso Liquido do Produto Não Informado')
-            existsOrError(info.product.package_height, 'Altura da Embalagem do Produto Não Informada')
-            existsOrError(info.product.package_weight, 'Peso Bruto do Produto Não Informado')
-            existsOrError(info.product.package_length, 'Comprimento da Embalagem do Produto Não Informado')
-            existsOrError(info.product.package_width, 'Largura da Embalagem do Produto Não Informado')
+            existsOrError(product.name, 'Nome Não Informado')
+            existsOrError(product.short_description, 'Descrição Curta Não Informada')
+            existsOrError(product.long_description, 'Descrição Longa Não Informada')
+            existsOrError(product.shortVideoUrl, 'Url do video curto Não Informada')
+            existsOrError(product.longVideoUrl, 'Url do video longo Não Informada')
+            existsOrError(product.categoryId, 'Categoria Não Informada')
+            existsOrError(product.stock, 'Quantidade em estoque Não Informado')
+            existsOrError(product.product_height, 'Altura do Produto Não informada')
+            existsOrError(product.product_width, 'Largura do Produto Não Informada')
+            existsOrError(product.product_length, 'Comprimento do Produto Não Informado')
+            existsOrError(product.product_weight, 'Peso Liquido do Produto Não Informado')
+            existsOrError(product.package_height, 'Altura da Embalagem do Produto Não Informada')
+            existsOrError(product.package_weight, 'Peso Bruto do Produto Não Informado')
+            existsOrError(product.package_length, 'Comprimento da Embalagem do Produto Não Informado')
+            existsOrError(product.package_width, 'Largura da Embalagem do Produto Não Informado')
 
-            if (info.product.id) {
+
+
+            if (req.params.id) {
                 console.log("entrou")
+                console.log(req.params.id)
+                try {
+                    console.log(product)
+                    await app.db('products')
+                        .update(product)
+                        .where({ id: req.params.id })
 
-                app.db('products')
-                    .update(info.product)
-                    .where({ id: info.product.id })
+                    if (images.images.length > 0) {
+                        console.log("entrou aqui", product)
+                        const nonNullImages = images.images.filter(url => url !== null && url !== undefined && url !== '');
+
+                        const imageData = nonNullImages.map(url => ({
+                            product_id: req.params.id,
+                            url: url
+                        }));
+                        console.log(imageData)
+
+                        await app.db('product_images').where({ product_id: req.params.id }).del();
+
+                        await app.db('product_images').insert(imageData)
+                    }
+
+                    console.log("deu bom")
+                } catch (error) {
+                    res.status(500).send("Erro ao atualizar Produto", error)
+                }
+
             } else {
-                const [productId] = await app.db('products')
-                    .insert(info.product).returning("id")
-                info.product.id = productId.id
-            }
-            if (info.images.length > 0 && info.images != null) {
-                const imageData = info.images.map(url => ({
-                    product_id: info.product.id,
-                    url: url
-                }));
-                console.log(imageData)
-                
-                await app.db('product_images').where({product_id: info.product.id }).del();
-                
-                await app.db('product_images').insert(imageData)
+                try {
+                    const [productId] = await app.db('products')
+                        .insert(product).returning("id")
+                    product.id = productId.id
+                    console.log(product.id)
+
+                    if (images.images.length > 0) {
+                        console.log("entrou aqui", product)
+                        const nonNullImages = images.images.filter(url => url !== null && url !== undefined && url !== '');
+
+                        const imageData = nonNullImages.map(url => ({
+                            product_id: product.id,
+                            url: url
+                        }));
+
+                        console.log(imageData)
+
+                        await app.db('product_images').where({ product_id: product.id }).del();
+
+                        await app.db('product_images').insert(imageData)
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
             }
 
-            res.status(200).send();
+
+            res.status(200).send("Produto Salvo Com sucesso")
 
         } catch (msg) {
-            res.status(400).send(msg)
+            res.status(500).send(msg)
         }
-
-
-
-
 
     }
 
     const remove = async (req, res) => {
         try {
-            const rowsDeleted = await app.db('products')
-                .where({ id: req.params.id }).del()
-            existsOrError(rowsDeleted, 'Produto Não Foi Encontrado.')
-
-            res.status(200).send()
+          await app.db('product_images').where({ product_id: req.params.id }).del();
+      
+          const rowsDeleted = await app.db('products').where({ id: req.params.id }).del();
+          existsOrError(rowsDeleted, 'Produto Não Foi Encontrado.');
+      
+          res.status(200).send("Produto Excluido Com Sucesso!");
         } catch (msg) {
-            res.status(500).send(msg)
+          res.status(500).send(msg);
         }
-    }
+      };
+      
 
-    const limit = 10
+    // const limit = 10
+    // const get = async (req, res) => {
+    //     const page = req.query.page || 1
+
+    //     const result = await app.db('products').count('id').first()
+    //     const count = parseInt(result.count)
+
+    //     app.db('products')
+    //         .select('id', 'name', 'imageUrl', 'stock')
+    //         .then(products => res.json(products))
+    //         .catch(err => res.status(500).send(err))
+    // }
+
+
     const get = async (req, res) => {
-        const page = req.query.page || 1
+        try {
+            const products = await app.db('products')
+                .select('*')
+                .groupBy('id')  // Agrupa os resultados por produto
+                .orderBy('id')
 
-        const result = await app.db('products').count('id').first()
-        const count = parseInt(result.count)
+            const images = await app.db('product_images')
+                .select('product_id', 'url')
 
-        app.db('products')
-            .select('id', 'name', 'imageUrl', 'stock')
-            .then(products => res.json(products))
-            .catch(err => res.status(500).send(err))
-    }
+
+
+
+            //   Mapeia os resultados para formatar o array de imagens
+            const formattedProducts = products.map(product => {
+
+                const productImages = images
+                    .filter(image => image.product_id === product.id)
+                    .map(image => image.url);
+
+                return {
+                    ...product,
+                    images: productImages,
+                };
+            });
+
+            res.json(formattedProducts);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    };
+
+
 
     const getById = async (req, res) => {
         app.db('products')
