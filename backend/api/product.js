a817d3e29c30e92255fa14ded2dce97907b50a34
RamonBarros <89661742+RamonBarros@users.noreply.github.com>
2024-04-29 10:47:00 -0300

    Implementação de Validação das entradas da api de Produtos.

--- backend/api/product.js
+++ backend/api/product.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 module.exports = app => {
     const { existsOrError, notExistsOrError } = app.api.validation
 
@@ -12,88 +14,175 @@ module.exports = app => {
         const product = Object.fromEntries(keys.slice(0, -1).map(key => [key, info[key]]));
         const images = { [lastKey]: lastValue };
 
+
         // console.log(product);
         console.log(images.images);
+        console.log(info)
+        const schema = Joi.object({
+            id: Joi.number().messages({
+                'number.base': 'Nome Não Informado',
+                'number.positive': 'Id deve ser um número positivo'
+            }),
+            name: Joi.string().required().messages({
+                'any.required': 'Nome Não Informado'
+            }),
+            short_description: Joi.string().required().messages({
+                'any.required': 'Descrição Curta Não Informada'
+            }),
+            long_description: Joi.string().required().messages({
+                'any.required': 'Descrição Longa Não Informada'
+            }),
+            imageUrl: Joi.string().required().messages({
+                'any.required': 'imageurl'
+            }),
+            shortVideoUrl: Joi.string().required().messages({
+                'any.required': 'Url do vídeo curto Não Informada'
+            }),
+            longVideoUrl: Joi.string().required().messages({
+                'any.required': 'Url do vídeo longo Não Informada'
+            }),
+            categoryId: Joi.number().integer().positive().required().messages({
+                'any.required': 'Categoria Não Informada',
+                'number.base': 'Categoria deve ser um número',
+                'number.integer': 'Categoria deve ser um número inteiro',
+                'number.positive': 'Categoria deve ser um número positivo'
+            }),
+            stock: Joi.number().integer().positive().required().messages({
+                'any.required': 'Quantidade em estoque Não Informada',
+                'number.base': 'Quantidade em estoque deve ser um número',
+                'number.integer': 'Quantidade em estoque deve ser um número inteiro',
+                'number.positive': 'Quantidade em estoque deve ser um número positivo'
+            }),
+            price: Joi.number().integer().positive().required().messages({
+                'any.required': 'Preço Não Informado',
+                'number.base': 'Preço deve ser um número',
+                'number.integer': 'Preço deve ser um número inteiro',
+                'number.positive': 'Preço deve ser um número positivo'
+            }),
+            product_height: Joi.number().positive().required().messages({
+                'any.required': 'Altura do Produto Não informada',
+                'number.base': 'Altura do Produto deve ser um número',
+                'number.positive': 'Altura do Produto deve ser um número positivo'
+            }),
+            product_width: Joi.number().positive().required().messages({
+                'any.required': 'Largura do Produto Não Informada',
+                'number.base': 'Largura do Produto deve ser um número',
+                'number.positive': 'Largura do Produto deve ser um número positivo'
+            }),
+            product_length: Joi.number().positive().required().messages({
+                'any.required': 'Comprimento do Produto Não Informado',
+                'number.base': 'Comprimento do Produto deve ser um número',
+                'number.positive': 'Comprimento do Produto deve ser um número positivo'
+            }),
+            product_weight: Joi.number().positive().required().messages({
+                'any.required': 'Peso Liquido do Produto Não Informado',
+                'number.base': 'Peso Liquido do Produto deve ser um número',
+                'number.positive': 'Peso Liquido do Produto deve ser um número positivo'
+            }),
+            package_height: Joi.number().positive().required().messages({
+                'any.required': 'Altura da Embalagem do Produto Não Informada',
+                'number.base': 'Altura da Embalagem do Produto deve ser um número',
+                'number.positive': 'Altura da Embalagem do Produto deve ser um número positivo'
+            }),
+            package_weight: Joi.number().positive().required().messages({
+                'any.required': 'Peso Bruto do Produto Não Informado',
+                'number.base': 'Peso Bruto do Produto deve ser um número',
+                'number.positive': 'Peso Bruto do Produto deve ser um número positivo'
+            }),
+            package_length: Joi.number().positive().required().messages({
+                'any.required': 'Comprimento da Embalagem do Produto Não Informado',
+                'number.base': 'Comprimento da Embalagem do Produto deve ser um número',
+                'number.positive': 'Comprimento da Embalagem do Produto deve ser um número positivo'
+            }),
+            package_width: Joi.number().positive().required().messages({
+                'any.required': 'Largura da Embalagem do Produto Não Informado',
+                'number.base': 'Largura da Embalagem do Produto deve ser um número',
+                'number.positive': 'Largura da Embalagem do Produto deve ser um número positivo'
+            })
+        });
 
         try {
-            existsOrError(product.name, 'Nome Não Informado')
-            existsOrError(product.short_description, 'Descrição Curta Não Informada')
-            existsOrError(product.long_description, 'Descrição Longa Não Informada')
-            existsOrError(product.shortVideoUrl, 'Url do video curto Não Informada')
-            existsOrError(product.longVideoUrl, 'Url do video longo Não Informada')
-            existsOrError(product.categoryId, 'Categoria Não Informada')
-            existsOrError(product.stock, 'Quantidade em estoque Não Informado')
-            existsOrError(product.product_height, 'Altura do Produto Não informada')
-            existsOrError(product.product_width, 'Largura do Produto Não Informada')
-            existsOrError(product.product_length, 'Comprimento do Produto Não Informado')
-            existsOrError(product.product_weight, 'Peso Liquido do Produto Não Informado')
-            existsOrError(product.package_height, 'Altura da Embalagem do Produto Não Informada')
-            existsOrError(product.package_weight, 'Peso Bruto do Produto Não Informado')
-            existsOrError(product.package_length, 'Comprimento da Embalagem do Produto Não Informado')
-            existsOrError(product.package_width, 'Largura da Embalagem do Produto Não Informado')
+            // existsOrError(product.name, 'Nome Não Informado')
+            // if (images.images.length === 0) {
+            //     console.log('deu Erro')
+            //     throw new Error({message:'Insira Pelo Menos uma Imagem para Salvar o Produto'});
+            // }
 
+            const { error } = await schema.validateAsync(product);
+
+            if (error) {
+                console.error(error.message)
+                throw new Error(error.message);
+            } else {
+                console.log("OK")
+            }
 
 
             if (req.params.id) {
                 console.log("entrou")
                 console.log(req.params.id)
-                try {
-                    console.log(product)
-                    await app.db('products')
-                        .update(product)
-                        .where({ id: req.params.id })
 
-                    if (images.images.length > 0) {
-                        console.log("entrou aqui", product)
-                        const nonNullImages = images.images.filter(url => url !== null && url !== undefined && url !== '');
+                console.log(product)
+                await app.db('products')
+                    .update(product)
+                    .where({ id: req.params.id }).catch(error=>{
+                        throw new Error(error)
+                    })
 
-                        const imageData = nonNullImages.map(url => ({
-                            product_id: req.params.id,
-                            url: url
-                        }));
-                        console.log(imageData)
+                if (images.images.length > 0) {
+                    console.log("entrou aqui", product)
+                    const nonNullImages = images.images.filter(url => url !== null && url !== undefined && url !== '');
 
-                        await app.db('product_images').where({ product_id: req.params.id }).del();
+                    const imageData = nonNullImages.map(url => ({
+                        product_id: req.params.id,
+                        url: url
+                    }));
+                    console.log(imageData)
 
-                        await app.db('product_images').insert(imageData)
-                    }
+                    await app.db('product_images').where({ product_id: req.params.id }).del().catch(error=>{
+                        throw new Error(error.message)
+                    });
 
-                } catch (error) {
-                    console.error(error)
-                    res.status(500).send("Erro ao atualizar Produto", error)
+                    await app.db('product_images').insert(imageData).catch(error=>{
+                        throw new Error(error.message)
+                    })
                 }
+
+
 
             } else {
-                try {
-                    const [productId] = await app.db('products')
-                        .insert(product).returning("id")
-                    product.id = productId.id
-                    console.log(product.id)
 
-                    if (images.images.length > 0) {
-                        const nonNullImages = images.images.filter(url => url !== null && url !== undefined && url !== '');
+                const [productId] = await app.db('products')
+                    .insert(product).returning("id")
+                product.id = productId.id
+                console.log(product.id)
 
-                        const imageData = nonNullImages.map(url => ({
-                            product_id: product.id,
-                            url: url
-                        }));
+                if (images.images.length > 0) {
+                    const nonNullImages = images.images.filter(url => url !== null && url !== undefined && url !== '');
 
-                        console.log(imageData)
+                    const imageData = nonNullImages.map(url => ({
+                        product_id: product.id,
+                        url: url
+                    }));
 
-                        await app.db('product_images').where({ product_id: product.id }).del();
+                    console.log(imageData)
 
-                        await app.db('product_images').insert(imageData)
-                    }
-                } catch (error) {
-                    console.log(error)
+                    await app.db('product_images').where({ product_id: product.id }).del().catch(error=>{
+                        throw new Error(error.message)
+                    });;
+
+                    await app.db('product_images').insert(imageData).catch(error=>{
+                        throw new Error(error.message)
+                    });
                 }
-            }
 
+            }
 
             res.status(200).send("Produto Salvo Com sucesso")
 
-        } catch (msg) {
-            res.status(500).send(msg)
+        } catch (error) {
+            console.log(error.message)
+            return res.status(500).send({ message: error.message })
         }
 
     }
@@ -161,20 +250,20 @@ module.exports = app => {
 
         try {
             const product = await app.db('products')
-                .select('*') 
+                .select('*')
                 .where('id', '=', req.params.id)
                 .first();
 
             const images = await app.db('product_images')
                 .select('url')
-                .where('product_id','=', req.params.id)
+                .where('product_id', '=', req.params.id)
 
             //   Mapeia os resultados para formatar o array de imagens
 
-           const formatedData = {
-                    product: product,
-                    images: images
-                };
+            const formatedData = {
+                product: product,
+                images: images
+            };
 
             res.json(formatedData);
         } catch (err) {
@@ -185,7 +274,7 @@ module.exports = app => {
     const getProductById = async (req, res) => {
         try {
             const product = await app.db('products')
-                .select('*') 
+                .select('*')
                 .where('id', '=', req.params.id)
                 .first();
             res.json(product);
@@ -242,5 +331,5 @@ module.exports = app => {
 
 
 
-    return { save, remove, get, getById, getByCategoryCart, searchBar,getProductById }
+    return { save, remove, get, getById, getByCategoryCart, searchBar, getProductById }
 }
